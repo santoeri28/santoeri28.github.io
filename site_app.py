5a0893dbaafe0f8cb00caf9ba24faa2bb1370964
santoeri28 <103185474+santoeri28@users.noreply.github.com>
2024-05-25 15:07:21 -0300

    site_app.py

--- site_app.py
+++ site_app.py
@@ -7,14 +7,15 @@ import pandas as pd
 mydataset = "https://raw.githubusercontent.com/santoeri28/santoeri28.gsd.key.io/main/file_3Dcsv2.csv"
 df = pd.read_csv(mydataset, sep=';', on_bad_lines='skip')
 
+# Initialize the Dash app (normally under the name 'app')
+app = dash.Dash(__name__)
+server = app.server
+
 # Create the Plotly figure
 fig = px.scatter_3d(df, x='Ri - Di', y='Ri + Di', z='Cosine Sim', size='Ri + Di', color='W_cluster',
                     hover_data=['Description'])
 fig.update_layout(scene_zaxis_type="log")
 
-# Initialize the Dash app (normally under the name 'app')
-app = dash.Dash(__name__)
-server = app.server
 # Define the layout of the app
 app.layout = html.Div([
     dcc.Graph(
