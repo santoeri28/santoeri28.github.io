8e0b33a71e1fb2796afdb8f853af9a2ceb746494
santoeri28 <103185474+santoeri28@users.noreply.github.com>
2024-05-25 14:31:58 -0300

    Add files via upload

---
+++ site_app.py
@@ -0,0 +1,28 @@
+import dash
+from dash import html, dcc
+import plotly.express as px
+import pandas as pd
+
+# Load your DataFrame
+mydataset = "https://raw.githubusercontent.com/santoeri28/santoeri28.gsd.key.io/main/file_3Dcsv2.csv"
+df = pd.read_csv(mydataset, sep=';', on_bad_lines='skip')
+
+# Create the Plotly figure
+fig = px.scatter_3d(df, x='Ri - Di', y='Ri + Di', z='Cosine Sim', size='Ri + Di', color='W_cluster',
+                    hover_data=['Description'])
+fig.update_layout(scene_zaxis_type="log")
+
+# Initialize the Dash app (normally under the name 'app')
+app = dash.Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div([
+    dcc.Graph(
+        id='3d-scatter-plot',
+        figure=fig
+    )
+])
+
+# This line allows the app to be run on a development server
+if __name__ == '__main__':
+    app.run_server(debug=True)
